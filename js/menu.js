ab99baddad0c14966965d0f890c9629e9e018fad
Becous <becousgame@gmail.com>
2023-12-26 18:44:09 +0200

    ше чутка

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuButton = document.querySelector(".mobile-menu-button");
-  const header = document.querySelector(".header");
+const menu = document.querySelector(".menu");
+const body = document.body;
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
-  menuButton.addEventListener("click", function () {
-    header.classList.toggle("open");
-  });
-});
-
-function scrollToElement(elementId) {
-  var element = document.getElementById(elementId);
-  if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    body.classList.remove("no-scroll");
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    body.classList.add("no-scroll");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
 }
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener("click", toggleMenu);
+});
